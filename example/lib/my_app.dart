05ad457356534d789134ef4e180c4525e2e0ae8e
Muhammad Kamel <mu7ammadkamel@hotmail.com>
2025-12-07 02:23:37 +0200

    feat: make `designSize` optional in `ScreenUtilPlusInit` and `configure` by using a default.

---
+++ example/lib/my_app.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil_plus/flutter_screenutil_plus.dart';
+import 'package:flutter_screenutil_plus_example/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Set the fit size (Find your UI design, look at the dimensions of the device screen and fill it in, unit in dp)
+    return ScreenUtilPlusInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      // Use builder only if you need to use library outside ScreenUtilPlusInit context
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter ScreenUtil Plus Example',
+          // Use ResponsiveTheme to automatically scale all text styles
+          theme: ResponsiveTheme.fromTheme(
+            ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
+          ),
+          home: child,
+        );
+      },
+      child: const HomePage(),
+    );
+  }
+}
